471f7e6914146ed4d16c2efbaab8355b0560d6c0
hyatt <hyatt.lifelog@gmail.com>
2023-11-11 22:34:55 +0900

    commit api route

---
+++ pages/api/run-puppeteer.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+  ) {
+  const { URL = 'https://twitter.com/n0bisuke' } = req.query;
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(URL); //URLにアクセス
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      title: document.title
+    };
+  });
+
+  console.log('Dimensions:', dimensions); 
+
+  await browser.close();
+
+  //アクセスしたページのタイトルを取得
+  res.send(`${URL}のページタイトルは「${dimensions.title}」だよー!`);
+}
---
+++ pages/api/run-puppeteer2.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+let chrome = {};
+let puppeteer = {};
+
+//puppeteer main process
+const run = async (puppeteer, chrome={}, URL) => {
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(URL);
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      title: document.title,
+      deviceScaleFactor: window.devicePixelRatio
+    };
+  });
+  await browser.close();
+
+  return dimensions;
+}
+
+if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
+  //Vercel
+  chrome = require('chrome-aws-lambda');
+  puppeteer = require('puppeteer-core');
+}else{
+  //Local Test
+  puppeteer = require('puppeteer');
+
+  const URL = `https://www.yahoo.com/`;
+  run(puppeteer,{},URL).then(res => console.log(res));
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+  ) {
+  const { URL='https://www.yahoo.com/' } = req.query;
+  const dimensions = await run(puppeteer, chrome, URL);
+  res.send(`${URL}'s title is'${dimensions.title}!`);
+}
