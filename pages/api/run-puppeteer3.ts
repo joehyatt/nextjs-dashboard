cf940ee82d884317e24a2a7062ac846ceb8af92e
hyatt <hyatt.lifelog@gmail.com>
2023-11-12 12:49:51 +0900

    refactering

--- pages/api/run-puppeteer3.ts
+++ pages/api/run-puppeteer3.ts
@@ -2,56 +2,58 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 let chrome = {};
 let puppeteer = {};
+let options = {}
 
-//puppeteer main process
 const run = async (puppeteer: any, chrome:any={}, URL: string) => {
 
-  console.log("run start")
-  const browser = await puppeteer.launch({
-    args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-    executablePath: await chrome.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-    ),
-    headless: chrome.headless,
-    ignoreHTTPSErrors: true,
-  });
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION){
+    // Production --------------------------------------------- //
+    console.log("production start")
+    chrome = require('@sparticuz/chromium-min');
+    puppeteer = require('puppeteer-core');
+    options = {
+      args: [...chrome?.args, '--hide-scrollbars', '--disable-web-security'],
+      executablePath: await chrome.executablePath(
+        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+      ),
+      headless: chrome.headless,
+      ignoreHTTPSErrors: true,
+    }
+    // Production --------------------------------------------- //
+  } else {
+    // Local --------------------------------------------- //
+    console.log("local start")
+    puppeteer = require('puppeteer');
+    options = {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    }
+    // Local --------------------------------------------- //
+  }
 
+  const browser = await puppeteer.launch(options)
   const page = await browser.newPage();
-  console.log("after newPage set")
   await page.goto(URL);
-  console.log("after goto url")
-  
-  // Get the "viewport" of the page, as reported by the page.
+
   const dimensions = await page.evaluate(() => {
     return {
       title: document.title,
       deviceScaleFactor: window.devicePixelRatio
     };
   });
+
   await browser.close();
   console.log(dimensions.title)
-
   return dimensions;
-}
-
-if(process.env.AWS_LAMBDA_FUNCTION_VERSION){
-  //Vercel
-  console.log("production start")
-  chrome = require('@sparticuz/chromium-min');
-  puppeteer = require('puppeteer-core');
-}else{
-  //Local Test
-  console.log("local start")
-  puppeteer = require('puppeteer');
-  const URL = `http://abehiroshi.la.coocan.jp/`;
-  run(puppeteer,{},URL).then(res => console.log(res));
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
+  // const { URL='https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=TOYSHDI&arrivalDate=2023-11-01&departureDate=2023-11-02' } = req.query;
   const { URL='http://abehiroshi.la.coocan.jp/' } = req.query;
   const dimensions = await run(puppeteer, chrome, URL as string);
-  res.send(`${URL}'s title is'${dimensions.title.toString('UTF-8')}!`);
+  res.send(`${URL}'s title is'${dimensions.title}!`);
 }
