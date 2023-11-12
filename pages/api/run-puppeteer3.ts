090f845099d672fd982f9c36405afda11db5dcec
hyatt <hyatt.lifelog@gmail.com>
2023-11-12 22:20:34 +0900

    update apis

--- pages/api/run-puppeteer3.ts
+++ pages/api/run-puppeteer3.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 let chrome = {};
 let puppeteer = {};
 let options = {};
-const searchUrl = "https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=TOYSHDI&arrivalDate=2023-11-01&departureDate=2023-11-02";
+// const searchUrl = "https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=TOYSHDI&arrivalDate=2023-11-01&departureDate=2023-11-02";
 
 const run = async (puppeteer: any, chrome:any={}, URL: string) => {
 
@@ -28,7 +28,7 @@ const run = async (puppeteer: any, chrome:any={}, URL: string) => {
     options = {
       args: chrome.args,
       executablePath: await chrome.executablePath,
-      headless: chrome.headless,
+      headless: false,
     }
     // Local --------------------------------------------- //
   }
@@ -37,24 +37,22 @@ const run = async (puppeteer: any, chrome:any={}, URL: string) => {
   const page = await browser.newPage();
   await page.goto(URL);
 
-  const dimensions = await page.evaluate(() => {
-    return {
-      title: document.title,
-      deviceScaleFactor: window.devicePixelRatio
-    };
+  const title = await page.evaluate(() => {
+    return  document.title;
   });
 
   await browser.close();
-  console.log(dimensions.title)
-  return dimensions;
+
+  console.log(title)
+  return title;
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
-  // const { URL='https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=TOYSHDI&arrivalDate=2023-11-01&departureDate=2023-11-02' } = req.query;
-  const { URL='http://abehiroshi.la.coocan.jp/' } = req.query;
-  const dimensions = await run(puppeteer, chrome, URL as string);
-  res.send(`${URL}'s title is'${dimensions.title}!`);
+  const { URL='https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=TOYSHDI&arrivalDate=2023-11-01&departureDate=2023-11-02' } = req.query;
+  // const { URL='http://abehiroshi.la.coocan.jp/' } = req.query;
+  const title = await run(puppeteer, chrome, URL as string);
+  res.send(`${URL}'s title is ${title}`);
 }
--- pages/api/test_hilton.ts
+++ pages/api/test_hilton.ts
@@ -1,69 +1,96 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type Prices = {
+    hotelId: string,
+    date: string,
+    price: string
+}
+
 let chrome = {};
 let puppeteer = {};
 let options = {};
-const searchUrl = "https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=TOYSHDI&arrivalDate=2023-11-01&departureDate=2023-11-02";
+// const searchUrl = "https://www.marriott.com/reservation/availabilitySearch.mi?isRateCalendar=true&propertyCode=OSAAL&isSearch=true&currency=&fromToDate_submit=12/07/2023&fromDate=12/06/2023&toDate=12/07/2023&toDateDefaultFormat=12/07/2023";
+// const searchUrl = "https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=TOYSHDI&arrivalDate=2023-11-01&departureDate=2023-11-02";
 
-const run = async (puppeteer: any, chrome:any={}, URL: string) => {
+const run = async (puppeteer: any, chrome:any={}) => {
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION){
-    // Production --------------------------------------------- //
-    console.log("production start")
-    chrome = require('@sparticuz/chromium-min');
-    puppeteer = require('puppeteer-core');
-    options = {
-      args: [...chrome?.args, '--hide-scrollbars', '--disable-web-security'],
-      executablePath: await chrome.executablePath(
-        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-      ),
-    //   headless: chrome.headless,
-      headless: false,
-      ignoreHTTPSErrors: true,
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION){
+        // Production --------------------------------------------- //
+        console.log("production start")
+        chrome = require('@sparticuz/chromium-min');
+        puppeteer = require('puppeteer-core');
+        options = {
+        args: [...chrome?.args, '--hide-scrollbars', '--disable-web-security'],
+        executablePath: await chrome.executablePath(
+            `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+        ),
+        headless: false,
+        ignoreHTTPSErrors: true,
+        }
+        // Production --------------------------------------------- //
+    } else {
+        // Local --------------------------------------------- //
+        console.log("local start")
+        puppeteer = require('puppeteer');
+        options = {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: false,
+        slowMo: 100,
+        }
+        // Local --------------------------------------------- //
     }
-    // Production --------------------------------------------- //
-  } else {
-    // Local --------------------------------------------- //
-    console.log("local start")
-    puppeteer = require('puppeteer');
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-    //   headless: chrome.headless,
-      headless: false,
-      slowMo: 100,
+
+    const browser = await puppeteer.launch(options)
+    const page = await browser.newPage();
+
+    const hotels = ["TYOHITW","HIJSHHI"];
+    const monthCount = 2;
+    // const baseUrl = "https://www.hilton.com/en/book/reservation/flexibledates/?";
+
+    for (let hotelNum = 0; hotelNum < hotels.length; hotelNum++) {
+        const hotelId = hotels[hotelNum];
+        for (let monthNum = 0; monthNum < monthCount; monthNum++) {
+            let currentMonth = new Date();
+            const searchMonth = new Date(currentMonth.setMonth(currentMonth.getMonth() + monthNum)).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit"}).replace('/', '-');
+            const searchUrl = `https://www.hilton.com/en/book/reservation/flexibledates/?ctyhocn=${hotelId}&arrivalDate=${searchMonth}-01&departureDate=${searchMonth}-02`;
+            
+            console.log(`collecting ${hotelId} : ${searchMonth} prices...`);
+
+            await page.goto(searchUrl);
+            const getNumOfDaysInMonth = function(year: number, month:number) {  
+                return new Date(year, month, 0).getDate();
+            };
+            const lastDay = getNumOfDaysInMonth(Number(searchMonth.slice(0,4)), Number(searchMonth.slice(-2)))
+            await page.waitForSelector("#flexibleDatesCalendar > div:nth-child(3) > div > div div[data-testid='flexDatesRoomRate'] > span", { hidden: true, timeout: 0 });
+            const priceList = await page.evaluate((hotelId: string, lastDay: number, searchMonth: string) => {
+                const priceList:Prices[] = []
+                for (let dayNum = 0; dayNum < lastDay; dayNum++) {
+                    const searchDate = ( '00' + (dayNum + 1) ).slice(-2);
+                    let price;
+                    if (document.querySelector(`button[data-testid='arrival-${searchMonth}-${searchDate}'] > div:nth-child(3) > div[data-testid='flexDatesRoomRate`)) {
+                        price = document.querySelector(`button[data-testid='arrival-${searchMonth}-${searchDate}'] > div:nth-child(3) > div[data-testid='flexDatesRoomRate`)?.innerText;
+                    } else if (document.querySelector(`button[data-testid='arrival-${searchMonth}-${searchDate}'] > div:nth-child(3) > div > span[data-testid='rateNotAvailable']`)) {
+                        price = "sold out"
+                    } else {
+                        price = "unavailable date"
+                    }
+                    priceList.push({hotelId, date:`${searchMonth}-${searchDate}`, price})
+                }
+                return priceList
+            },hotelId,lastDay,searchMonth);
+            console.log(priceList)
+        }
     }
-    // Local --------------------------------------------- //
-  }
 
-  const browser = await puppeteer.launch(options)
-  const page = await browser.newPage();
-  await page.goto(URL);
-//   await new Promise(resolve => setTimeout(resolve, 1000))
-  await page.waitForSelector("#flexibleDatesCalendar > div:nth-child(3) > div > div div[data-testid='flexDatesRoomRate'] > span", { hidden: true, timeout: 0 });
-
-//   const dimensions = await page.evaluate(() => {
-//     return {
-//       title: document.title,
-//       deviceScaleFactor: window.devicePixelRatio
-//     };
-//   });
-
-  const text = await page.evaluate(() => {
-    const month = document.querySelector("#flexibleDatesCalendar > div:nth-child(3) > div > div.lg\\:border-border.mb-4.lg\\:border-l.lg\\:border-t > div:nth-child(3) > div:nth-child(5) > div > button > div.overflow-hidden.lg\\:flex.lg\\:flex-1.lg\\:flex-col.lg\\:items-center.lg\\:justify-center > div.font-body.group-hover\\:text-text-inverse.text-center.text-xs.lg\\:text-2xl.lg\\:font-bold.\\[word-break\\:break-word\\].text-primary");
-    return month?.textContent;
-  });
-
-  await browser.close();
+    await browser.close();
   
-  console.log(text)
-  return text;
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
-  const text = await run(puppeteer, chrome, searchUrl);
-  res.send(`Month's title is ${text}!`);
+  await run(puppeteer, chrome);
+  res.send(`DONE!`);
 }
