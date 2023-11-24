7a50ff25e15c88b8b6e11477bb32fcf21d0aa044
hyatt <hyatt.lifelog@gmail.com>
2023-11-24 13:33:37 +0900

    watchlist fix

--- app/ui/watchlist/table.tsx
+++ app/ui/watchlist/table.tsx
@@ -21,31 +21,22 @@ export default async function WatchlistTable(
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
             {watchlist?.map((watchitem) => (
-              <div
-                key={watchitem.id}
-                className="mb-3 w-full rounded-md bg-white p-4 flex-row"
-              >
-                <p className="text-sm text-gray-500">{formatDateToLocal(watchitem.cid)}泊</p>
-                <div className="flex items-center justify-between pb-4">
-                  <div>
-                    <div className="mb-2 flex items-center">
-                      <p>{watchitem.hotel_name_jp}</p>
-                      <p>{formatCurrency(watchitem.basis)}</p>
-                    </div>
+              <div key={watchitem.id} className="mb-3 w-full rounded-md bg-white p-4 flex flex-row" >
+                <div className="flex flex-col w-2/3 items-left justify-between">
+                  <p className="text-lg">
+                    {formatDateToLocal(watchitem.cid)}泊
+                  </p>
+                  <div className="mb-2 flex items-center">
+                    <p>{watchitem.hotel_name_jp}</p>
+                  </div>
+                  <div className="mb-2 flex items-center">
+                    <p>{formatCurrency(watchitem.basis)}</p>
                   </div>
                 </div>
-                <div className="flex w-full items-center justify-between pt-4">
-                  <div>
-                    <p className="text-xl font-medium">
-                      {formatCurrency(watchitem.basis)} 
-                      {watchitem.rate !== null ? formatCurrency(watchitem.rate) : watchitem.exception}
-                    </p>
-                    <p>{watchitem.status}</p>
-                  </div>
-                  <div className="flex justify-end gap-2">
-                    <UpdateWatchitem id={watchitem.id} />
-                    <DeleteWatchitem id={watchitem.id} />
-                  </div>
+                <div className="flex w-1/3 flex-col items-center justify-between align-middle">
+                  <p className="text-xl font-medium align-middle">
+                    {watchitem.rate !== null ? formatCurrency(watchitem.rate) : watchitem.exception}
+                  </p>
                 </div>
               </div>
             ))}
@@ -54,11 +45,11 @@ export default async function WatchlistTable(
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
-                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  ホテル名
-                </th>
                 <th scope="col" className="px-3 py-5 font-medium">
                   チェックイン日
+                </th>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  ホテル名
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
                   基準価格
@@ -66,15 +57,15 @@ export default async function WatchlistTable(
                 <th scope="col" className="px-3 py-5 font-medium">
                   最新価格
                 </th>
-                <th scope="col" className="px-3 py-5 font-medium">
+                {/* <th scope="col" className="px-3 py-5 font-medium">
                   ステータス
-                </th>
+                </th> */}
                 {/* <th scope="col" className="px-3 py-5 font-medium">
                   価格取得日
                 </th> */}
-                <th scope="col" className="relative py-3 pl-6 pr-3">
+                {/* <th scope="col" className="relative py-3 pl-6 pr-3">
                   <span className="sr-only">Edit</span>
-                </th>
+                </th> */}
               </tr>
             </thead>
             <tbody className="bg-white">
@@ -83,20 +74,13 @@ export default async function WatchlistTable(
                   key={watchitem.id}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
-                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex items-center gap-3">
-                      {/* <Image
-                        src={invoice.image_url}
-                        className="rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${invoice.name}'s profile picture`}
-                      /> */}
-                      <p>{watchitem.hotel_name_jp}</p>
-                    </div>
-                  </td>
                   <td className="whitespace-nowrap px-3 py-3">
                     {formatDateToLocal(watchitem.cid)}
+                  </td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex items-center gap-3">
+                      <p>{watchitem.hotel_name_jp}</p>
+                    </div>
                   </td>
                   <td className="whitespace-nowrap px-3 py-3">
                     {formatCurrency(watchitem.basis)}
@@ -104,18 +88,18 @@ export default async function WatchlistTable(
                   <td className="whitespace-nowrap px-3 py-3">
                     {watchitem.rate !== null ? formatCurrency(watchitem.rate) : watchitem.exception}
                   </td>
-                  <td className="whitespace-nowrap px-3 py-3">
+                  {/* <td className="whitespace-nowrap px-3 py-3">
                     {watchitem.status}
-                  </td>
+                  </td> */}
                   {/* <td className="whitespace-nowrap px-3 py-3">
                     {watchitem.basis}
                     <InvoiceStatus status={invoice.status} />
                   </td> */}
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex justify-end gap-3">
+                    {/* <div className="flex justify-end gap-3">
                       <UpdateWatchitem id={watchitem.id} />
                       <DeleteWatchitem id={watchitem.id} />
-                    </div>
+                    </div> */}
                   </td>
                 </tr>
               ))}
