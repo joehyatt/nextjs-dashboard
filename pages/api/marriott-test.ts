9b264373aabf09d01132888494d12d4fd8f71ff7
hyatt <hyatt.lifelog@gmail.com>
2023-12-03 01:20:20 +0900

    cal search

--- pages/api/marriott-test.ts
+++ pages/api/marriott-test.ts
@@ -89,7 +89,7 @@ const captureRates = async (puppeteer: any, chrome:any={}) => {
         console.log(searchUrl);
 
         try{
-            await page.goto("https://www.marriott.com/search/findHotels.mi?fromDate=12/18/2023&toDate=12/19/2023");
+            await page.goto("https://www.marriott.com/reservation/availabilitySearch.mi?isRateCalendar=true&propertyCode=UKBFI&isSearch=true&currency=");
             await new Promise(resolve => setTimeout(resolve, 3000));
             const title = await page.title();
             console.log("page title: ",title);
