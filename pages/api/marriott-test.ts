b779cb9edf00b4b084f1a2060173e924c682b7ac
hyatt <hyatt.lifelog@gmail.com>
2023-12-03 02:13:38 +0900

    dcload

--- pages/api/marriott-test.ts
+++ pages/api/marriott-test.ts
@@ -95,7 +95,7 @@ const captureRates = async (puppeteer: any, chrome:any={}) => {
             //     page.goto("https://www.marriott.com/search/findHotels.mi?fromDate=12/19/2023&toDate=12/20/2023&destinationAddress.destination=Japan&view=list",{ waitUntil: 'networkidle0' }),
             //     page.waitForNavigation({ waitUntil: 'networkidle2' }),
             // ]);
-            await page.goto("https://www.marriott.com/search/findHotels.mi?fromDate=12/19/2023&toDate=12/20/2023&destinationAddress.destination=Japan&view=list",{ waitUntil: 'networkidle0' });
+            await page.goto("https://www.marriott.com/search/findHotels.mi?fromDate=12/19/2023&toDate=12/20/2023&destinationAddress.destination=Japan&view=list",{ "waitUntil":"domcontentloaded" });
             await new Promise(resolve => setTimeout(resolve, 3000));
             const title = await page.title();
             console.log("page title: ",title);
